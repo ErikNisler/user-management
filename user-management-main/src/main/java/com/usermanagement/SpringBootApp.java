a750b5a1e2658ea788d10dd3241dcfcb3e4dd2fe
Erik Nisler <erik.nisler@seznam.cz>
2024-12-13 17:29:16 +0100

    DB migration - localhost -> cloud

--- user-management-main/src/main/java/com/usermanagement/SpringBootApp.java
+++ user-management-main/src/main/java/com/usermanagement/SpringBootApp.java
@@ -1,7 +1,6 @@
 package com.usermanagement;
 
 import com.usermanagement.ui.App;
-import com.usermanagement.ui.Login;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
